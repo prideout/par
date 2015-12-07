70b5f962cc048ea77992345664b53dae4edf0105
Philip Rideout <philip@napofearth.com>
2015-12-06 19:28:27 -0800

    Update URL.

--- par_msquares.h
+++ par_msquares.h
@@ -6,7 +6,7 @@
 // Color images can be r8, rg16, rgb24, or rgba32. For a visual overview of
 // the API and all the flags, see:
 //
-//     https://....
+//     http://github.prideout.net/marching-cubes/
 //
 // The MIT License
 // Copyright (c) 2015 Philip Rideout
