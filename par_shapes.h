1e768082a7ff23a47725c71415d0f7edbd75def8
Philip Rideout <philiprideout@gmail.com>
2016-01-01 18:18:58 -0800

    Add par_shapes_create_rock.

--- par_shapes.h
+++ par_shapes.h
@@ -54,12 +54,14 @@ par_shapes_mesh* par_shapes_create_icosahedron();
 // Create a sphere from a subdivided icosahedron without normals or uvs.
 par_shapes_mesh* par_shapes_create_sphere(int nsubdivisions);
 
+// Create a rock shape that sits on the Y plane by displacing a sphere.
+par_shapes_mesh* par_shapes_create_rock(int seed, int nsubdivisions);
+
 // TBD, http://prideout.net/blog/?p=44
 typedef void (*par_shapes_fn)(float* const, float*);
 par_shapes_mesh* par_shapes_create_custom_parametric(par_shapes_fn, int slices,
     int stacks, int flags);
 par_shapes_mesh* par_shapes_create_tree(int seed, int flags);
-par_shapes_mesh* par_shapes_create_rock(int seed, int flags);
 par_shapes_mesh* par_shapes_create_octohedron();
 par_shapes_mesh* par_shapes_create_cube(); // for Cornell boxes
 
@@ -75,6 +77,7 @@ par_shapes_mesh* par_shapes_create_cube(); // for Cornell boxes
 #include <float.h>
 #include <string.h>
 #include <math.h>
+#include <errno.h>
 
 #define PAR_MIN(a, b) (a > b ? b : a)
 #define PAR_MAX(a, b) (a > b ? a : b)
@@ -89,6 +92,11 @@ static void par_shapes_private_plane(float* const uv, float* xyz);
 static void par_shapes_private_klein(float* const uv, float* xyz);
 static void par_shapes_private_cylinder(float* const uv, float* xyz);
 static void par_shapes_private_torus(float* const uv, float* xyz);
+
+struct osn_context;
+static int par_simplex_noise(int64_t seed, struct osn_context** ctx);
+static void par_simplex_noise_free(struct osn_context* ctx);
+static double par_simplex_noise2(struct osn_context* ctx, double x, double y);
 
 static par_shapes_fn par_shapes_functions[] = {
     par_shapes_private_sphere,
@@ -711,6 +719,301 @@ par_shapes_mesh* par_shapes_create_sphere(int nsubd)
         mesh->triangles[i] = i;
     }
     return mesh;
+}
+
+par_shapes_mesh* par_shapes_create_rock(int seed, int subd)
+{
+    par_shapes_mesh* mesh = par_shapes_create_sphere(subd);
+    struct osn_context* ctx;
+    par_simplex_noise(seed, &ctx);
+    for (int p = 0; p < mesh->npoints; p++) {
+        float* pt = mesh->points + p * 3;
+        float a = 0.25, f = 1.0;
+        double n = a * par_simplex_noise2(ctx, f * pt[0], f * pt[2]);
+        a *= 0.5; f *= 2;
+        n += a * par_simplex_noise2(ctx, f * pt[0], f * pt[2]);
+        par_shapes_scale3(pt, 1.0 + n);
+        if (pt[1] < 0) {
+            pt[1] = -pow(-pt[1], 0.5) / 2;
+        }
+    }
+    par_simplex_noise_free(ctx);
+    return mesh;
+}
+
+// -----------------------------------------------------------------------------
+// BEGIN OPEN SIMPLEX NOISE
+// -----------------------------------------------------------------------------
+
+#define STRETCH_CONSTANT_2D (-0.211324865405187)  // (1 / sqrt(2 + 1) - 1 ) / 2;
+#define SQUISH_CONSTANT_2D (0.366025403784439)  // (sqrt(2 + 1) -1) / 2;
+#define STRETCH_CONSTANT_3D (-1.0 / 6.0)  // (1 / sqrt(3 + 1) - 1) / 3;
+#define SQUISH_CONSTANT_3D (1.0 / 3.0)  // (sqrt(3+1)-1)/3;
+#define STRETCH_CONSTANT_4D (-0.138196601125011)  // (1 / sqrt(4 + 1) - 1) / 4;
+#define SQUISH_CONSTANT_4D (0.309016994374947)  // (sqrt(4 + 1) - 1) / 4;
+
+#define NORM_CONSTANT_2D (47.0)
+#define NORM_CONSTANT_3D (103.0)
+#define NORM_CONSTANT_4D (30.0)
+
+#define DEFAULT_SEED (0LL)
+
+struct osn_context {
+    int16_t* perm;
+    int16_t* permGradIndex3D;
+};
+
+#define ARRAYSIZE(x) (sizeof((x)) / sizeof((x)[0]))
+
+/*
+ * Gradients for 2D. They approximate the directions to the
+ * vertices of an octagon from the center.
+ */
+static const int8_t gradients2D[] = {
+    5, 2, 2, 5, -5, 2, -2, 5, 5, -2, 2, -5, -5, -2, -2, -5,
+};
+
+/*
+ * Gradients for 3D. They approximate the directions to the
+ * vertices of a rhombicuboctahedron from the center, skewed so
+ * that the triangular and square facets can be inscribed inside
+ * circles of the same radius.
+ */
+static const signed char gradients3D[] = {
+    -11, 4, 4, -4, 11, 4, -4, 4, 11, 11, 4, 4, 4, 11, 4, 4, 4, 11, -11, -4, 4,
+    -4, -11, 4, -4, -4, 11, 11, -4, 4, 4, -11, 4, 4, -4, 11, -11, 4, -4, -4, 11,
+    -4, -4, 4, -11, 11, 4, -4, 4, 11, -4, 4, 4, -11, -11, -4, -4, -4, -11, -4,
+    -4, -4, -11, 11, -4, -4, 4, -11, -4, 4, -4, -11,
+};
+
+/*
+ * Gradients for 4D. They approximate the directions to the
+ * vertices of a disprismatotesseractihexadecachoron from the center,
+ * skewed so that the tetrahedral and cubic facets can be inscribed inside
+ * spheres of the same radius.
+ */
+static const signed char gradients4D[] = {
+    3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3, -3, 1, 1, 1, -1, 3, 1, 1,
+    -1, 1, 3, 1, -1, 1, 1, 3, 3, -1, 1, 1, 1, -3, 1, 1, 1, -1, 3, 1, 1, -1, 1,
+    3, -3, -1, 1, 1, -1, -3, 1, 1, -1, -1, 3, 1, -1, -1, 1, 3, 3, 1, -1, 1, 1,
+    3, -1, 1, 1, 1, -3, 1, 1, 1, -1, 3, -3, 1, -1, 1, -1, 3, -1, 1, -1, 1, -3,
+    1, -1, 1, -1, 3, 3, -1, -1, 1, 1, -3, -1, 1, 1, -1, -3, 1, 1, -1, -1, 3, -3,
+    -1, -1, 1, -1, -3, -1, 1, -1, -1, -3, 1, -1, -1, -1, 3, 3, 1, 1, -1, 1, 3,
+    1, -1, 1, 1, 3, -1, 1, 1, 1, -3, -3, 1, 1, -1, -1, 3, 1, -1, -1, 1, 3, -1,
+    -1, 1, 1, -3, 3, -1, 1, -1, 1, -3, 1, -1, 1, -1, 3, -1, 1, -1, 1, -3, -3,
+    -1, 1, -1, -1, -3, 1, -1, -1, -1, 3, -1, -1, -1, 1, -3, 3, 1, -1, -1, 1, 3,
+    -1, -1, 1, 1, -3, -1, 1, 1, -1, -3, -3, 1, -1, -1, -1, 3, -1, -1, -1, 1, -3,
+    -1, -1, 1, -1, -3, 3, -1, -1, -1, 1, -3, -1, -1, 1, -1, -3, -1, 1, -1, -1,
+    -3, -3, -1, -1, -1, -1, -3, -1, -1, -1, -1, -3, -1, -1, -1, -1, -3,
+};
+
+static double extrapolate2(
+    struct osn_context* ctx, int xsb, int ysb, double dx, double dy)
+{
+    int16_t* perm = ctx->perm;
+    int index = perm[(perm[xsb & 0xFF] + ysb) & 0xFF] & 0x0E;
+    return gradients2D[index] * dx + gradients2D[index + 1] * dy;
+}
+
+static inline int fastFloor(double x)
+{
+    int xi = (int) x;
+    return x < xi ? xi - 1 : xi;
+}
+
+static int allocate_perm(struct osn_context* ctx, int nperm, int ngrad)
+{
+    if (ctx->perm)
+        free(ctx->perm);
+    if (ctx->permGradIndex3D)
+        free(ctx->permGradIndex3D);
+    ctx->perm = (int16_t*) malloc(sizeof(*ctx->perm) * nperm);
+    if (!ctx->perm)
+        return -ENOMEM;
+    ctx->permGradIndex3D =
+        (int16_t*) malloc(sizeof(*ctx->permGradIndex3D) * ngrad);
+    if (!ctx->permGradIndex3D) {
+        free(ctx->perm);
+        return -ENOMEM;
+    }
+    return 0;
+}
+
+static int par_simplex_noise(int64_t seed, struct osn_context** ctx)
+{
+    int rc;
+    int16_t source[256];
+    int i;
+    int16_t* perm;
+    int16_t* permGradIndex3D;
+
+    *ctx = (struct osn_context*) malloc(sizeof(**ctx));
+    if (!(*ctx))
+        return -ENOMEM;
+    (*ctx)->perm = NULL;
+    (*ctx)->permGradIndex3D = NULL;
+
+    rc = allocate_perm(*ctx, 256, 256);
+    if (rc) {
+        free(*ctx);
+        return rc;
+    }
+
+    perm = (*ctx)->perm;
+    permGradIndex3D = (*ctx)->permGradIndex3D;
+
+    for (i = 0; i < 256; i++)
+        source[i] = (int16_t) i;
+    seed = seed * 6364136223846793005LL + 1442695040888963407LL;
+    seed = seed * 6364136223846793005LL + 1442695040888963407LL;
+    seed = seed * 6364136223846793005LL + 1442695040888963407LL;
+    for (i = 255; i >= 0; i--) {
+        seed = seed * 6364136223846793005LL + 1442695040888963407LL;
+        int r = (int) ((seed + 31) % (i + 1));
+        if (r < 0)
+            r += (i + 1);
+        perm[i] = source[r];
+        permGradIndex3D[i] =
+            (short) ((perm[i] % (ARRAYSIZE(gradients3D) / 3)) * 3);
+        source[r] = source[i];
+    }
+    return 0;
+}
+
+static void par_simplex_noise_free(struct osn_context* ctx)
+{
+    if (!ctx)
+        return;
+    if (ctx->perm) {
+        free(ctx->perm);
+        ctx->perm = NULL;
+    }
+    if (ctx->permGradIndex3D) {
+        free(ctx->permGradIndex3D);
+        ctx->permGradIndex3D = NULL;
+    }
+    free(ctx);
+}
+
+static double par_simplex_noise2(struct osn_context* ctx, double x, double y)
+{
+    // Place input coordinates onto grid.
+    double stretchOffset = (x + y) * STRETCH_CONSTANT_2D;
+    double xs = x + stretchOffset;
+    double ys = y + stretchOffset;
+
+    // Floor to get grid coordinates of rhombus (stretched square) super-cell
+    // origin.
+    int xsb = fastFloor(xs);
+    int ysb = fastFloor(ys);
+
+    // Skew out to get actual coordinates of rhombus origin. We'll need these
+    // later.
+    double squishOffset = (xsb + ysb) * SQUISH_CONSTANT_2D;
+    double xb = xsb + squishOffset;
+    double yb = ysb + squishOffset;
+
+    // Compute grid coordinates relative to rhombus origin.
+    double xins = xs - xsb;
+    double yins = ys - ysb;
+
+    // Sum those together to get a value that determines which region we're in.
+    double inSum = xins + yins;
+
+    // Positions relative to origin point.
+    double dx0 = x - xb;
+    double dy0 = y - yb;
+
+    // We'll be defining these inside the next block and using them afterwards.
+    double dx_ext, dy_ext;
+    int xsv_ext, ysv_ext;
+
+    double value = 0;
+
+    // Contribution (1,0)
+    double dx1 = dx0 - 1 - SQUISH_CONSTANT_2D;
+    double dy1 = dy0 - 0 - SQUISH_CONSTANT_2D;
+    double attn1 = 2 - dx1 * dx1 - dy1 * dy1;
+    if (attn1 > 0) {
+        attn1 *= attn1;
+        value += attn1 * attn1 * extrapolate2(ctx, xsb + 1, ysb + 0, dx1, dy1);
+    }
+
+    // Contribution (0,1)
+    double dx2 = dx0 - 0 - SQUISH_CONSTANT_2D;
+    double dy2 = dy0 - 1 - SQUISH_CONSTANT_2D;
+    double attn2 = 2 - dx2 * dx2 - dy2 * dy2;
+    if (attn2 > 0) {
+        attn2 *= attn2;
+        value += attn2 * attn2 * extrapolate2(ctx, xsb + 0, ysb + 1, dx2, dy2);
+    }
+
+    if (inSum <= 1) {  // We're inside the triangle (2-Simplex) at (0,0)
+        double zins = 1 - inSum;
+        if (zins > xins ||
+            zins >
+                yins) {  //(0,0) is one of the closest two triangular vertices
+            if (xins > yins) {
+                xsv_ext = xsb + 1;
+                ysv_ext = ysb - 1;
+                dx_ext = dx0 - 1;
+                dy_ext = dy0 + 1;
+            } else {
+                xsv_ext = xsb - 1;
+                ysv_ext = ysb + 1;
+                dx_ext = dx0 + 1;
+                dy_ext = dy0 - 1;
+            }
+        } else {  //(1,0) and (0,1) are the closest two vertices.
+            xsv_ext = xsb + 1;
+            ysv_ext = ysb + 1;
+            dx_ext = dx0 - 1 - 2 * SQUISH_CONSTANT_2D;
+            dy_ext = dy0 - 1 - 2 * SQUISH_CONSTANT_2D;
+        }
+    } else {  // We're inside the triangle (2-Simplex) at (1,1)
+        double zins = 2 - inSum;
+        if (zins < xins ||
+            zins <
+                yins) {  //(0,0) is one of the closest two triangular vertices
+            if (xins > yins) {
+                xsv_ext = xsb + 2;
+                ysv_ext = ysb + 0;
+                dx_ext = dx0 - 2 - 2 * SQUISH_CONSTANT_2D;
+                dy_ext = dy0 + 0 - 2 * SQUISH_CONSTANT_2D;
+            } else {
+                xsv_ext = xsb + 0;
+                ysv_ext = ysb + 2;
+                dx_ext = dx0 + 0 - 2 * SQUISH_CONSTANT_2D;
+                dy_ext = dy0 - 2 - 2 * SQUISH_CONSTANT_2D;
+            }
+        } else {  //(1,0) and (0,1) are the closest two vertices.
+            dx_ext = dx0;
+            dy_ext = dy0;
+            xsv_ext = xsb;
+            ysv_ext = ysb;
+        }
+        xsb += 1;
+        ysb += 1;
+        dx0 = dx0 - 1 - 2 * SQUISH_CONSTANT_2D;
+        dy0 = dy0 - 1 - 2 * SQUISH_CONSTANT_2D;
+    }
+
+    // Contribution (0,0) or (1,1)
+    double attn0 = 2 - dx0 * dx0 - dy0 * dy0;
+    if (attn0 > 0) {
+        attn0 *= attn0;
+        value += attn0 * attn0 * extrapolate2(ctx, xsb, ysb, dx0, dy0);
+    }
+
+    // Extra Vertex
+    double attn_ext = 2 - dx_ext * dx_ext - dy_ext * dy_ext;
+    if (attn_ext > 0) {
+        attn_ext *= attn_ext;
+        value += attn_ext * attn_ext *
+                 extrapolate2(ctx, xsv_ext, ysv_ext, dx_ext, dy_ext);
+    }
+
+    return value / NORM_CONSTANT_2D;
 }
 
 #undef PAR_MIN
